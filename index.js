015845bdd36d3e9880d3421b87974a53a8e45cbc
Chris <christopher.brighton@netnatives.co.uk>
2022-11-01 12:45:51 +0000

    expose funcs / classes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const ModelHandler = require('./src/handler')
 const { parse } = require('./src/parser')
+const { HttpStatusError } = require('./src/errors')
+const ModelHandler = require('./src/handler')
 
 module.exports = {
   ModelHandler,
   parse,
+  HttpStatusError,
+  ModelHandler,
 }
